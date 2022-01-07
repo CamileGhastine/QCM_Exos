713d68453bac7f821ae9b066b8f876ffbc616261
CamileGhastine <camileghastine@hotmail.com>
2022-01-07 11:18:03 +0100

    Implement the method rollDice()
    throw ramdomly 5 dices X times (X is determine by the parameter of the
    methode)
    the methode return an array of array

--- exercice2/Yam.php
+++ exercice2/Yam.php
@@ -4,25 +4,35 @@ class Yam
 {
     private array $diceRoll;
 
-    public function rollDice(): array
+    public function rollDice(int $numberOfThrow): array
     {
+        $diceRoll = [];
+
+        for ($j = 1; $j <= $numberOfThrow; $j++) {
+           
+            $oneRoll = [];
+
+            for ($i = 1; $i <= 5; $i++) {
+                $oneRoll[] = rand(1, 6);
+            }
+
+            $diceRoll[] = $oneRoll;
+        }
+        return $diceRoll;
     }
 
     public function countYam(array $rollDice): int
     {
-
     }
 
     public function countFourOfAKinD(array $rollDice): int
     {
-        
     }
 
     public function countThreeOfAKind(array $rollDice): int
     {
-        
     }
 }
 
 $a = new Yam;
-$a->test();
+var_dump($a->rollDice(2));
